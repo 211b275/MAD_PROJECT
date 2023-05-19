032395de9224478909ae5b19f217f8d24cb9c796
“[Sarthak <[211B275@juetguna.in]>
2023-05-19 18:32:15 +0530

    i have commited changes over here.

--- app/src/main/java/com/example/shivam_my_friend_sri_arora/CreatePasswardActivity.java
+++ app/src/main/java/com/example/shivam_my_friend_sri_arora/CreatePasswardActivity.java
@@ -1,22 +1,23 @@
 package com.example.shivam_my_friend_sri_arora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class CreatePasswardActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreatePasswardActivity extends AppCompatActivity
+{
 
     EditText editText1, editText2;
     Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_passward);
 
@@ -24,31 +25,41 @@ public class CreatePasswardActivity extends AppCompatActivity {
         editText2 = findViewById(R.id.editTextTextPassword2);
         button = findViewById(R.id.button);
 
-        button.setOnClickListener(v -> {
+        button.setOnClickListener(v ->
+        {
             String text1 = editText1.getText().toString();
             String text2 = editText2.getText().toString();
 
 
-            if(text1.equals("") || text2.equals("")){
-                //there s no password
+            if(text1.equals("") || text2.equals(""))
+            {
+
+                //there s no password present here and we are no quick need of it.
                 Toast.makeText(CreatePasswardActivity.this,"No password entered!", Toast.LENGTH_SHORT).show();
-            } else {
-                if(text1.equals(text2)){
-                    //save the password
+            }
+            else
+            {
+                if(text1.equals(text2))
+                {
+
+                    //save the password for future preference.
                     SharedPreferences settings = getSharedPreferences("PREFS", 0);
                     SharedPreferences.Editor editor = settings.edit();
                     editor.putString("password",text1);
                     editor.apply();
-                    // enter the app
+
+                    // enter the app once
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
-                } else {
-                    //there is no match on
+                }
+                else
+                {
+
+                    //there is no match on found.
                     Toast.makeText(CreatePasswardActivity.this, "Passwords doesn't match!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
 }
